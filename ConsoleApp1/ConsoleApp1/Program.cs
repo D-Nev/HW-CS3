b5b0086e7868753ff8542f53580e80df22b71f2d
lamensakk@gmail.com <lamensakk@gmail.com>
2025-02-23 21:02:33 +0200

    w1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int[] array = { 1, 2, 3, 4, 5, 6, 2, 3, 4 };
+            //int chetny = array.Count(x => x % 2 == 0);
+            //int nechetny = array.Count(x => x % 2 != 0);
+            //int unique = array.Distinct().Count();
+            //Console.WriteLine($"Четные: {chetny}, Нечетные: {nechetny}, Уникальные: {unique}");
+
+            //2
+            //Console.Write("Введите число: ");
+            //int Vod = Convert.ToInt32(Console.ReadLine());
+            //int[] arr = { 5, 8, 2, 6, 9, 3, 7 };
+            //int count = arr.Count(x => x < Vod);
+            //Console.WriteLine($"Чисел меньше {Vod}: {count}");
+
+            //3
+
+
+            //4
+            //int[] arr1 = { 1, 2, 3, 4, 5 };
+            //int[] arr2 = { 3, 4, 5, 6, 7 };
+            //int[] common = arr1.Intersect(arr2).ToArray();
+            //Console.WriteLine("Общие элементы: " + string.Join(", ", common));
+
+            //5
+            //int[,] matrix = { { 3, 7, 1 }, { 9, 2, 6 }, { 4, 8, 5 } };
+            //int min = matrix.Cast<int>().Min();
+            //int max = matrix.Cast<int>().Max();
+            //Console.WriteLine($"Минимум: {min}, Максимум: {max}");
+
+            //6
+
+            //7
+
+            //8
+            //Console.Write("Введите предложение: ");
+            //string slovo = Console.ReadLine()!;
+            //int count = slovo.Count(c => "aeiouAEIOU".Contains(c));
+            //Console.WriteLine($"Количество гласных: {count}");
+
+            //9
+        }
+    }
+}
